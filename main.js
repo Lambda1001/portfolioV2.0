82e407d4b0cf18c843b45f645e37cfc3fa488f1d
KevinDar1001 <kevin.andeka@darasa.app>
2025-02-14 12:27:28 +0300

    feat; 14 Feb

--- main.js
+++ main.js
@@ -28,3 +28,13 @@ tippy('#laravel', {
 tippy('#sql', {
     content: 'SQL',
 })
+
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    const element = document.getElementById('embed-iframe');
+    const options = {
+        uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
+      };
+    const callback = (EmbedController) => {};
+    IFrameAPI.createController(element, options, callback);
+  };
+  
